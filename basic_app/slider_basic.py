664c68ef2b5db68f3b469128ff8acd959d545a8b
Eva Huang <evah.data@gmail.com>
2018-05-07 15:31:29 -0500

    drop down menus

---
+++ basic_app/slider_basic.py
@@ -0,0 +1,86 @@
+from bokeh.io import curdoc
+
+# Create plots and widgets
+
+# Add callbacks
+
+# Arrange plots and widgets in layouts
+
+# Perform necessary imports
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+
+# Create a new plot: plot
+plot = figure()
+
+# Add a line to the plot
+plot.line([1,2,3,4,5],[2,5,4,6,7])
+
+# Add the plot to the current document
+curdoc().add_root(plot)
+
+
+# Perform the necessary imports
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox
+from bokeh.models import Slider
+
+# Create a slider: slider
+slider = Slider(title='my slider', start=0, end=10, step=0.1, value=2)
+
+# Create a widgetbox layout: layout
+layout = widgetbox(slider)
+
+# Add the layout to the current document
+curdoc().add_root(layout)
+
+
+# Perform necessary imports
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox
+from bokeh.models import Slider
+
+# Create first slider: slider1
+slider1 = Slider(title='slider1', start=0, end=10, step=0.1, value=2)
+
+# Create second slider: slider2
+slider2 = Slider(title='slider2', start=10, end=100, step=1, value=20)
+
+# Add slider1 and slider2 to a widgetbox
+layout = widgetbox(slider1, slider2)
+
+from bokeh.layouts import widgetbox
+
+# Create ColumnDataSource: source
+source = ColumnDataSource(data={'x':x, 'y':y})
+
+# Add a line to the plot
+plot.line('x', 'y', source=source)
+
+
+####### CALL BACK ####################
+# Define a callback function: callback
+def callback(attr, old, new):
+
+    # Read the current value of the slider: scale
+    scale = slider.value
+
+    # Compute the updated y using np.sin(scale/x): new_y
+    new_y = np.sin(scale/x)
+
+    # Update source with the new data values
+    source.data = {'x': x, 'y': new_y}
+
+# Attach the callback to the 'value' property of slider
+slider.on_change('value', callback)
+
+# Create layout and add to current document
+layout = column(widgetbox(slider), plot)
+curdoc().add_root(layout)
+
+# Create a column layout: layout
+layout = column(widgetbox(slider), plot)
+
+# Add the layout to the current document
+curdoc().add_root(layout)
+
