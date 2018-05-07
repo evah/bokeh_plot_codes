e84d52e216c35bd34984a51134a1a41e6b06d44d
Eva Huang <evah.data@gmail.com>
2018-05-07 10:26:45 -0500

    adding legend + hover

---
+++ annotations_guides/legends1.py
@@ -0,0 +1,31 @@
+# Add the first circle glyph to the figure p
+p.circle('fertility', 'female_literacy', source=latin_america, size=10, color='red', legend='Latin America')
+
+# Add the second circle glyph to the figure p
+p.circle('fertility', 'female_literacy', source=africa, size=10, color='blue', legend='Africa')
+
+# Specify the name of the output_file and show the result
+output_file('fert_lit_groups.html')
+
+# Assign the legend to the bottom left: p.legend.location
+p.legend.location = 'bottom_left'
+
+# Fill the legend background with the color 'lightgray': p.legend.background_fill_color
+p.legend.background_fill_color ='lightgray'
+
+# Specify the name of the output_file and show the result
+output_file('fert_lit_groups.html')
+
+# Import HoverTool from bokeh.models
+from bokeh.models import HoverTool
+
+# Create a HoverTool object: hover
+hover = HoverTool(tooltips=[('Country','@Country')])
+
+# Add the HoverTool object to figure p
+p.add_tools(hover)
+
+# Specify the name of the output_file and show the result
+output_file('hover.html')
+show(p)
+
